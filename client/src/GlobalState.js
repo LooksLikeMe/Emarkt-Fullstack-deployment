01cbd4e1a3295f6a085e17718550ea696ecdef7b
Mikhael <madenne39@gmail.com>
2022-01-03 22:16:56 +0200

    add authorization user and check on admin

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,5 +1,6 @@
 import React, {createContext, useState, useEffect} from "react";
 import ProductsAPI from "./api/ProductsAPI";
+import UserAPI from "./api/UserAPI";
 import axios from "axios";
 
 export const GlobalState = createContext()
@@ -8,21 +9,22 @@ export const DataProvider = ({children}) => {
     const [token, setToken] = useState(false)
 
     const refreshToken = async () => {
-        const token = await axios.get('/user/refresh_token')
+        const res = await axios.get('/user/refresh_token')
 
-        console.log(token)
+        setToken(res.data.accesstoken)
     }
     
     useEffect(() => {
         const firstLogin = localStorage.getItem('firstLogin')
         
-        refreshToken()
+        firstLogin && refreshToken()
     }, [])
 
     ProductsAPI()
     const state = {
         token: [token, setToken],
-        productsAPI: ProductsAPI()
+        productsAPI: ProductsAPI(),
+        UserAPI: UserAPI(token)
     }
     return (
         <GlobalState.Provider value={state}>
--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -1,12 +1,48 @@
-import React, { useContext, useState } from 'react'
-import { GlobalState } from '../../GlobalState'
-import Menu from './icon/bars-solid.svg'
-import Close from './icon/times-solid.svg'
-import Cart from './icon/shopping-cart-solid.svg'
-import { Link } from 'react-router-dom'
+import React, { useContext, useState } from "react";
+import { GlobalState } from "../../GlobalState";
+import Menu from "./icon/bars-solid.svg";
+import Close from "./icon/times-solid.svg";
+import Cart from "./icon/shopping-cart-solid.svg";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Header() {
-  const value = useContext(GlobalState)
+  const state = useContext(GlobalState);
+  console.log(state)
+  const [isLogged, setIsLogged] = state.UserAPI.isLogged;
+  const [isAdmin, setIsAdmin] = state.UserAPI.isAdmin;
+
+  const adminRouter = () => {
+    return (
+      <>
+        <li>
+          <Link to="/create_product">Create Product</Link>
+        </li>
+        <li>
+          <Link to="/category">Categories</Link>
+        </li>
+      </>
+    );
+  };
+  const logoutUser = async () => {
+    await axios.get('/user/logout')
+    localStorage.clear()
+    setIsAdmin(false)
+    setIsLogged(false)
+  }
+
+  const loggedRouter = () => {
+    return (
+      <>
+        <li>
+          <Link to="/history">History</Link>
+        </li>
+        <li>
+          <Link to="/" onClick={logoutUser}>Logout</Link>
+        </li>
+      </>
+    );
+  };
   return (
     <header>
       <div className="menu">
@@ -14,28 +50,37 @@ function Header() {
       </div>
       <div className="logo">
         <h1>
-          <Link to="/">EMarkt</Link>
+          <Link to="/">{isAdmin ? "Admin" : "DevAT Shop"}</Link>
         </h1>
       </div>
       <ul>
         <li>
-          <Link to="/">Products</Link>
+          <Link to="/"> {isAdmin ? "Products" : "Shop"}Products</Link>
         </li>
-        <li>
-          <Link to="/login">Login ‹› Register</Link>
-        </li>
+        {isAdmin && adminRouter()}{" "}
+        {isLogged ? (
+          loggedRouter()
+        ) : (
+          <li>
+            <Link to="/login">Login ‹› Register</Link>
+          </li>
+        )}
         <li>
           <img src={Close} alt="" width="30" className="menu" />
         </li>
       </ul>
-      <div className="cart-icon">
-        <span>0</span>
-        <Link to="/cart">
-          <img src={Cart} alt="" width="30" />
-        </Link>
-      </div>
+      {isAdmin ? (
+        ""
+      ) : (
+        <div className="cart-icon">
+          <span>0</span>
+          <Link to="/cart">
+            <img src={Cart} alt="" width="30" />
+          </Link>
+        </div>
+      )}
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
